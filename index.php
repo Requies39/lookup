42e5ac9a5884c93d702892a4f271eaa3297bc1be
Requies39 <hayeknabil@hotmail.com>
2016-05-17 19:10:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<script src="//fast.eager.io/OTvYvc36-K.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>MOBACAT · League of Legends Search and Stats</title>
+    
+    <script type="text/javascript">
+    if ((screen.width<=1280)) {
+    window.location.replace('http://google.com');
+    }
+    else {
+      window.location.replace('#');
+    }
+    </script>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:500,300,400' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header class="main-header">
+    <div class="top-header">
+        <div class="inner">
+            <div class="mobacat">
+                <a href="#"><img src="test.png"></img></a>
+            </div>
+            <nav class="nav">
+                    <div class="navtext">
+                        <a href="/search">Search</a>
+                        <span class="navspace"></span>
+                        <a href="/lobbyhelper">Lobby Helper</a>
+                        <span class="navspace"></span>
+                        <a href="/futureitembuilder">Future Item Builder</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div style="height: 55px;"></div>
+    <div class="landing-wrap">
+        <h1 class="banner">League of Legends</h1>
+    <form class="form-inline" role="form" action="index.php" method="get">
+        <div class="user-select">
+
+        <div class="form-group form-group-lg">
+            <input size="48" placeholder="Search for a summoner" maxlength="50" class="form-control" name="user" type="text">
+        </div>
+
+        <div class="form-group form-custom-shape form-group-lg">
+            <select class="form-control" name="region">
+            <option value="br">BR</option>
+            <option value="eune">EUNE</option>
+            <option value="euw">EUW</option>
+            <option value="lan">LAN</option>
+            <option value="las">LAS</option>
+            <option value="na">NA</option>
+            <option value="oce">OCE</option>
+            <option value="ru">RU</option>
+            <option value="tr">TR</option>
+            <option value="kr">KR</option>
+            </select>           
+        </div>
+    </div>
+    <div class="form-btn">
+        <span class="multisearch">
+        <input type="submit" value="Multi Search" class="link-button"></input>
+    </span>
+        <span class="space"></span>
+        <span class="search">
+        <input type="submit" value="Search" class="link-button"></input>
+    </span>
+    </div>
+    <div class="donate"><a href="/donate"><i class="fa fa-paypal"></i> Donate</a></div>
+</div>
+</form>
+</header>
+<div class="content">
+<h1 class="freetoplay">Free-to-play Champions:</h1>
+<div class="rotations">
+<span class="avatar-box"><img height="63px" width="63px" alt="Bard" title="Bard" class="image" src="icons/Bard_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Darius" title="Darius" class="image" src="icons/Darius_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Diana" title="Diana" class="image" src="icons/Diana_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Kindred" title="Kindred" class="image" src="icons/Kindred_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Lucian" title="lucian" class="image" src="icons/Lucian_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Lux" title="Lux" class="image" src="icons/Lux_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Miss Fortune" title="Miss Fortune" class="image" src="icons/MissFortune_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Nunu" title="Nunu" class="image" src="icons/Nunu_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Sion" title="Sion" class="image" src="icons/Sion_Square_0.png"></span>
+<span class="avatar-box"><img height="63px" width="63px" alt="Soraka" title="Soraka" class="image" src="icons/Soraka_Square_0.png"></span>
+</div>
+<div class="paragraph">This week's free to play champion rotation includes <b>Bard</b>, <b>Darius</b>, <b>Diana</b>, <b>Kindred</b>, <b>Lucian</b>, <b>Lux</b>, <b>Miss Fortune</b>, <b>Nunu</b>, <b>Sion</b>, and <b>Soraka</b>.<br> These champions will be available for free play starting October 27th.</div>
+</div>
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright &copy Mobacat.com, 2015. All rights to their respective owner(s).</p><br>
+    <small>MOBACAT.COM isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.<br> 
+League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</small></div>
+<br>
+
+    <div class="social-links">
+<a class="fa fa-twitter fa-2x" data-event="twitter" data-label="footer" href="https://twitter.com/mobacat" target="_blank" style="display:inline-block;"></a>
+<span class="footerspace"></span>
+<a class="fa fa-facebook-official fa-2x" data-event="facebook" data-label="footer" href="https://www.facebook.com/mobacat" target="_blank" style="display:inline-block;"></a>
+</div>
+<span class="links">
+    <center><a href="/about">About us</a> · <a href="/contact">Contact us</a> · <a href="/report">Report a problem</a> · <a href="/report">Donate</a> · <a href="/report">Terms of use</a></center>
+</span>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
